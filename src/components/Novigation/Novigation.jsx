6e48e0bed4e396e3b539d787c0e91a6585a6b761
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-03-04 20:18:13 +0200

    15

--- src/components/Novigation/Novigation.jsx
+++ src/components/Novigation/Novigation.jsx
@@ -1,25 +1,21 @@
 
 // import { Suspense } from "react";
 // import { Outlet } from "react-router-dom";
-import { Container, Header, Link} from '../Novigation/Novigation.styled.js';
+import { Header, Link} from '../Novigation/Novigation.styled.js';
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
 
  const Navigation = () => {
   return (
-    <Container>
-      <Header>
+     <Header>
         
         <div>
-          <Link to="/goit-react-hw-05-movies" end>Home</Link>
-          <Link to="/goit-react-hw-05-movies/movies">Movies</Link>
+          <Link to="goit-react-hw-05-movies/" end>Home</Link>
+          {/* <Link to="goit-react-hw-05-movies/movies">Movies</Link> */}
           
         </div>
       </Header>
-      {/* <Suspense fallback={<div>Loading page...</div>}>
-        <Outlet />
-      </Suspense> */}
-    </Container>
+   
   );
 };
 export default  Navigation

3c2abbbf430bfa60035ee93a2d559387a58305a1
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-03-05 17:36:08 +0200

    14

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,6 @@ export const App = () => {
 
   <Route path="movies" element={<Movies/>}/>
   <Route path="movies/:movieId" element={<MovieDetails/>}>
-    
     <Route path="cast" element={<Cast/>}/>
     <Route path="reviews" element={<Reviews/>}/>
 
